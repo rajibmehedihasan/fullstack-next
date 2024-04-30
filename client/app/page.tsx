2d65f23491c04613710001b5068a15e4af152dfe
Mehedi Hasan <mehedi.hasan2724@gmail.com>
2024-04-30 15:24:15 +0600

    Apply API method

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,41 @@
-import { Button, Input } from "@/components/shared";
+"use client";
+import { Button } from "@/components/shared";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+    const [mgs, setMessage] = useState("");
+
+    useEffect(() => {
+        try {
+            const fetchData = async () => {
+                const response = await fetch("/api/example", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ key: "value" }), // Replace with your request body
+                });
+
+                if (!response.ok) {
+                    throw new Error("Failed to fetch data");
+                }
+
+                const { message } = await response.json();
+                console.log(message);
+                setMessage(message);
+                // alert(message);
+            };
+
+            fetchData();
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    }, []);
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <Button />
-            <Input />
+            <h1 className="text-black">h1: {mgs}</h1>
         </main>
     );
 }
